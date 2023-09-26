3dfbec6e5cd652145edda69a7837bbd233ca7306
Lâm Tiên Hưng <Hungltse170216@fpt.edu.vn>
2023-09-27 01:06:00 +0700

    Update Player's Movement
    Creating Player's Walking animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public bool isMoving;
 
+    private Animator animator;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -18,24 +24,39 @@ public class PlayerController : MonoBehaviour
     private void PlayerMovement()
     {
         // Get input from the player
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
 
         // Calculate the movement vector
         Vector3 movement = Vector3.zero;
 
-        if (Mathf.Abs(horizontalInput) > Mathf.Abs(verticalInput))
+
+        if (Mathf.Abs(moveX) > Mathf.Abs(moveY))
         {
             // Moving primarily horizontally
-            movement = new Vector3(horizontalInput, 0f, 0f).normalized * moveSpeed * Time.deltaTime;
+            movement = new Vector3(moveX, 0f, 0f).normalized * moveSpeed * Time.deltaTime;
         }
         else
         {
             // Moving primarily vertically
-            movement = new Vector3(0f, verticalInput, 0f).normalized * moveSpeed * Time.deltaTime;
+            movement = new Vector3(0f, moveY, 0f).normalized * moveSpeed * Time.deltaTime;
         }
 
         // Move the character's Transform directly
         transform.Translate(movement);
+
+        // Check if the player is moving
+        isMoving = (movement.magnitude > 0f);
+
+        if (isMoving)
+        {
+            animator.SetFloat(nameof(moveX), moveX);
+            animator.SetFloat(nameof(moveY), moveY);
+            animator.SetBool(nameof(isMoving), isMoving);
+        }
+        else if (!isMoving)
+        {
+            animator.SetBool(nameof(isMoving), isMoving);
+        }
     }
 }
