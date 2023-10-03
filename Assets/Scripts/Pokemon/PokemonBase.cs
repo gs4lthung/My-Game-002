d9867838608e4654df9ba4f354db292b7f0211b8
Lâm Tiên Hưng <Hungltse170216@fpt.edu.vn>
2023-10-04 01:42:49 +0700

    Create a PokemonBase.cs
    Create a ScriptableObject class "PokemonBase.cs". It includes sprites, data and base stats of the pokemon.

---
+++ Assets/Scripts/Pokemon/PokemonBase.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Pokemon", menuName = "Pokemon/Create a new Pokemon")]
+public class PokemonBase : ScriptableObject
+{
+    [SerializeField] string name;
+
+    [TextArea]
+    [SerializeField] int description;
+
+    [SerializeField] Sprite frontSprite;
+    [SerializeField] Sprite backSprite;
+
+    [SerializeField] PokemonType type1;
+    [SerializeField] PokemonType type2;
+
+    // BASE STATS
+    [SerializeField] int maxHP;
+    [SerializeField] int attack;
+    [SerializeField] int defense;
+    [SerializeField] int spAttack;
+    [SerializeField] int spDefense;
+    [SerializeField] int speed;
+}
+
+public enum PokemonType
+{
+    Normal,
+    Fire,
+    Water,
+    Electric,
+    Grass,
+    Ice,
+    Fighting,
+    Poison,
+    Ground,
+    Flying,
+    Psychic,
+    Bug,
+    Rock,
+    Ghost,
+    Dragon,
+    Dark,
+    Steel,
+    Fairy
+}
